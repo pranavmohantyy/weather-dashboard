3d5c5665b0d67182fac14a976ffda6050cc8d5f1
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-25 00:52:22 -0700

    add temperature line chart

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HourlyForecast from './HourlyForecast';
 import WeeklyForecast from './WeeklyForecast';
+import TemperatureChart from './TemperatureChart';
 
 function CurrentWeather({ data }) {
   const { temperature, feels_like, humidity, windspeed, weathercode } = data.current_weather;
@@ -19,9 +20,9 @@ function CurrentWeather({ data }) {
       <p>Feels Like: {feels_like}°C</p>
       <p>Humidity: {humidity}%</p>
       <p>Wind: {windspeed} m/s</p>
-      <p>Condition: {weatherIcons[weathercode] || '🌈'}</p>
+      <p>Condition: {weatherIcons[weathercode] || 'N/A'}</p>
+      <TemperatureChart hourly={data.hourly} />
       <HourlyForecast hourly={data.hourly} />
-      <WeeklyForecast weekly={data.daily} />
     </div>
   );
 }
---
+++ src/TemperatureChart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function TemperatureChart({ hourly }) {
+  const maxTemp = Math.max(...hourly.map(hour => hour.temperature));
+  const minTemp = Math.min(...hourly.map(hour => hour.temperature));
+
+  return (
+    <svg width="100%" height="100">
+      <polyline
+        fill="none"
+        stroke="blue"
+        strokeWidth="2"
+        points={hourly.map((hour, index) => `${index * 10},${100 - ((hour.temperature - minTemp) / (maxTemp - minTemp)) * 100}`).join(' ')}
+      />
+      {hourly.map((hour, index) => (
+        <text key={index} x={index * 10} y="90" fontSize="10" textAnchor="middle">{hour.temperature}°C</text>
+      ))}
+    </svg>
+  );
+}
+
+export default TemperatureChart;
